7abf41a22d7f848204d5b866e3cf527574c7b448
ayomijpeg <ayomitomiwa53@gmail.com>
2025-03-25 15:42:01 +0100

    update1

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,12 +1,13 @@
 "use client";
- import { usePathname } from 'next/navigation';
- import clsx from 'clsx';
- import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import Link from 'next/link';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -29,7 +30,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-           className={clsx(
+            className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathName === link.href,
